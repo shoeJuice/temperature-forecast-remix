a81c1f0ea2753780ae63d7408f4fc08d3ddf1c7a
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-01-28 15:48:04 -0500

    Features a background that changes dynamically depending on the forecast

--- src/App.js
+++ src/App.js
@@ -14,15 +14,26 @@ import {keyframes} from '@emotion/react'
 import {css} from '@emotion/css'
 import styles from './styles/background.module.css'
 function App() {
-  
+  const [weather, setWeather] = React.useState()
+
+  React.useEffect(() => {
+    fetch('https://pro.openweathermap.org/data/2.5/forecast?lat=41.141&lon=-73.264&exclude=minutely,hourly&appid=69216bc1e255a60480a846fcb5004876&units=imperial')
+        .then(response => response.json())
+        .then(response => setWeather(response.list[0].weather[0]['main']))
+
+    console.log(weather)
+  }, [])
   return (
     <Flex
       alignItems='center'
       justifyContent='center'
       height='100vh'
-      
     >
-      <div className={styles.backgroundModule}></div>
+      <div className={styles.backgroundModule}><div className={css`
+      background: url(https://source.unsplash.com/random/2400x1801/?nature,${weather});
+      height: 100%;
+      width: 100%;
+      `}></div></div>
       <DisplayContainer />
     </Flex>
   );
