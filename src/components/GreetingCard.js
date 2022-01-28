9f100636de2a8ae2d521f18733598b516d05fa43
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-01-28 16:00:43 -0500

    Displays current date

--- src/components/GreetingCard.js
+++ src/components/GreetingCard.js
@@ -3,13 +3,24 @@ import {Box, Text} from '@chakra-ui/react'
 
 
 const GreetingCard = () => {
+  const dateObject = new Date(Date.now());
+  const [day, setDate] = React.useState()
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+  React.useEffect(() => {
+    console.log(dateObject.toLocaleDateString('en-US', options))
+    let dateFullString = dateObject.toLocaleDateString('en-US', options)
+    setDate(dateFullString)
+  
+  }, [])
   return (
         <Box
             textAlign="center"
         >
             <Text
                 fontSize={50}
+                marginBottom={-5}
             >Hello, Remy</Text>
+            <Text>Today is {day} </Text>
         </Box>
       );
 };
